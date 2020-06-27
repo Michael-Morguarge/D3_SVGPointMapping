e9756e53273efa72bb07ac84ab438b4324268c38
Michael Morguarge <mikeyjm14@gmail.com>
2020-06-27 16:46:02 -0400

    hosting to github pages

--- JS/svg.js
+++ JS/svg.js
@@ -1,3 +1,17 @@
+const aObject = document.getElementByClassName('.fit')[0];
+
+aObject.addEventListener("load", function() {
+    let svgDoc = aObject.contentDocument;
+    console.log(svgDoc);    
+    let land = aObject.contentDocument;
+    let menu = $('.context');
+
+    land.addEventListener('mousedown', ({ offsetX, offsetY }) => {
+        menu.style.top = offsetY + 'px';
+        menu.style.left = offsetX + 'px';
+    });
+});
+
 var svg = function(width, height, idName) {
     let projection =
         d3.geoEquirectangular().center([0,0]).scale([width/(2* Math.PI)]).translate([width/2, height/2]);
