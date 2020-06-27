1871dad4da5366cf2555de94e33f934c1eaf7e2c
Michael Morguarge <mikeyjm14@gmail.com>
2020-06-27 18:46:50 -0400

    Testing on Github

--- JS/svg.js
+++ JS/svg.js
@@ -1,38 +1,40 @@
-const aObject = document.getElementByClassName('.fit')[0];
+$(function(){
+    // $('.fit').on('load', function(){
+        const aObject = $('.fit');
 
-aObject.addEventListener("load", function() {
-    let svgDoc = aObject.contentDocument;
-    console.log(svgDoc);    
-    let land = aObject.contentDocument;
-    let menu = $('.context');
+        let svgDoc = document.getElementsByClassName('fit')[0];
+        console.log(svgDoc);    
+        let land = document.getElementsByClassName('fit')[0].contentDocument.getElementsByClassName('land');
+        let menu = $('.context');
 
-    land.addEventListener('mousedown', ({ offsetX, offsetY }) => {
-        menu.style.top = offsetY + 'px';
-        menu.style.left = offsetX + 'px';
-    });
-});
+        land.on('mousedown', ({ offsetX, offsetY }) => {
+            menu.style.top = offsetY + 'px';
+            menu.style.left = offsetX + 'px';
+        });
+    // })
 
-var svg = function(width, height, idName) {
-    let projection =
-        d3.geoEquirectangular().center([0,0]).scale([width/(2* Math.PI)]).translate([width/2, height/2]);
-    
-    let path = d3.geoPath().Projection(projection);
-
-    let zoom = d3.zoom().on("zoom",
-        function() {
-            _transform_ = d3.event.transform;
-            land.attr("transform", "translate(" + [_transform_.x, _transform_.y] + ")scale(" + _transform_.k + ")");
-        }
-    );
-
-    let minZoom;
-    let maxZoom;
-    
-    function GetBox(selection) {
-        selection.each(function(data){ data.bbox = this.GetBBox() });
-    }
-
-    function InitiateZoom() {
+    var svg = function(width, height, idName) {
+        let projection =
+            d3.geoEquirectangular().center([0,0]).scale([width/(2* Math.PI)]).translate([width/2, height/2]);
         
+        let path = d3.geoPath().Projection(projection);
+
+        let zoom = d3.zoom().on("zoom",
+            function() {
+                _transform_ = d3.event.transform;
+                land.attr("transform", "translate(" + [_transform_.x, _transform_.y] + ")scale(" + _transform_.k + ")");
+            }
+        );
+
+        let minZoom;
+        let maxZoom;
+        
+        function GetBox(selection) {
+            selection.each(function(data){ data.bbox = this.GetBBox() });
+        }
+
+        function InitiateZoom() {
+            
+        }
     }
-}
+})
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MappingWithSVG
-Test project for mapping with SVG. Implementations with D3, Raphael, and Maphael.
+
+Test project for mapping with SVG. Implementations with D3.
+
+## [D3 Test](HTML/index.html "Link for D3")
