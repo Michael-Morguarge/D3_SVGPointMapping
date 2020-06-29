716fa2173782a1115d233a24a7ca80868dac06c3
Michael Morguarge <mikeyjm14@gmail.com>
2020-06-29 14:57:22 -0400

    Working to make points show

--- JS/d3_svg.js
+++ JS/d3_svg.js
@@ -2,7 +2,7 @@ $(function(){
     window.setTimeout(function(){
         let land = $($('.fit')[0].contentDocument.getElementsByClassName('land'));
         
-        land.each(function(data) {
+        land.each(function() {
             if (this.id !== 'divider1' && this.id !== 'divider2') {
                 let menu = $('#context');
                 let content = $('#context').find('.data');
@@ -15,8 +15,7 @@ $(function(){
                     if (this.id !== 'divider1' && this.id !== 'divider2') {
                         menu.css('display', 'block');
                         content.html($(this).data().info); // html injection :(
-                        
-                        menu.css('top', (e.pageY - menu.height()+55) + 'px');
+                        menu.css('top', (e.pageY - menu.height()) + $('#title').height() + 'px');
                         menu.css('left', (e.pageX - (menu.width()/2)) + 'px');
                         $(this).addClass("selected");
                     }
@@ -25,8 +24,42 @@ $(function(){
         });    
     }, 1500);
 
-    d3.json('../Resources/Data/data.json', function(json) {
-        alert(json);
+    $('#data-view').change(function() {
+        function CreateSvgItem(tag, attrib) {
+            var element = document.createElementNS('http://www.w3.org/2000/svg', tag);
+            for (var k in attrib) {
+                element.setAttribute(k, attrib[k]);
+            }
+            return element;
+        }
+
+        let valueSelected = $(this).val() ?? null;
+        
+        if (valueSelected) {
+            d3.json(valueSelected).then(function(json) {
+                console.log(json.SetName);
+                
+                let theSVG = $($('.fit')[0].contentDocument.getElementsByTagName('svg')[0]);
+                let dataPoints = $(theSVG.find('#data-points'));
+                
+                if (dataPoints.length <= 0) {
+                    $(theSVG).append('<g/>');
+                    dataPoints = $($(theSVG.find('g')[1]));
+                    dataPoints.attr('id', 'data-points');
+                }
+                i = 0;
+
+                json.Data.forEach(function(value) {
+                    dataPoints.append(
+                        CreateSvgItem('circle', { cx: value.X, cy: value.Y, r: 5, class: "data-point" })
+                    );
+                    let circle = $(dataPoints.find('.data-point:last')[0]);
+                    circle.html(++i);
+                    
+                    //circle.click()
+                });
+            });
+        }
     });
 
     var svg = function(width, height, idName) {
